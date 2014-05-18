188acab2fbe7e64d81a66d460b796ade6984f5b2
Vladimir Tkachenko <vladimir.tkachenko.mk@gmai.com>
2014-05-18 20:26:29 +0300

    add dependencies / new sails version / add heavy-sails-angular yeoman clients for admin and site

--- testProject/config/adapters.js
+++ testProject/config/adapters.js
@@ -14,6 +14,18 @@
 
 module.exports.adapters = {
 
+    'default': 'mongo',
+
+    // sails v.0.9.0
+    mongo: {
+        module   : 'sails-mongo',
+        host     : 'localhost',
+        port     : 27017,
+        user     : '',
+        password : '',
+        database : 'testProject'
+    }
+
   // If you leave the adapter config unspecified 
   // in a model definition, 'default' will be used.
   /*
