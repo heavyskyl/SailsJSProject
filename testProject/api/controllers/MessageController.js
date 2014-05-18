96191b8e8e48e8c55786a68cddbb2cbdec0a18cf
Vladimir Tkachenko <vladimir.tkachenko.mk@gmai.com>
2014-05-18 20:58:54 +0300

    add dependencies / new sails version / add heavy-sails-angular yeoman clients for admin and site

---
+++ testProject/api/controllers/MessageController.js
@@ -0,0 +1,45 @@
+/**
+ * MessageController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+    
+  
+    /**
+    * Action blueprints:
+    *    `/chat/subscribe`
+    */
+    subscribe: function (req, res) {
+        Message.find(function(err, messages) {
+            if (err) {
+                throw err;
+            }
+
+            Message.subscribe(req.socket);
+            Message.subscribe(req.socket, messages);
+
+            res.send(messages);
+        });
+    },
+
+    /**
+    * Overrides for the settings in `config/controllers.js`
+    * (specific to MessageController)
+    */
+    _config: {}
+
+  
+};
