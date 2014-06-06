560ed7d707b5a07039302bf4dab4b82faeab56d8
heavyskyl <heavyskyl@yandex.ru>
2014-06-06 16:52:47 +0300

    create wrong login functionality - now user can login via web sockets with local strategy

--- testProject/api/controllers/AuthController.js
+++ testProject/api/controllers/AuthController.js
@@ -2,7 +2,7 @@
  * AuthController
  *
  * @module      :: Controller
- * @description	:: A set of functions called `actions`.
+ * @description    :: A set of functions called `actions`.
  *
  *                 Actions contain code telling Sails how to respond to a certain type of request.
  *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
@@ -15,23 +15,77 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
-var passport = require('passport');
+var passport = require('passport'),
+    bcrypt = require('bcrypt-nodejs/bCrypt.js');
 
 module.exports = {
 
-    'logout' : function(req, res) {
+    'logout': function (req, res) {
         req.logout();
         res.json({
-            success : true
+            success: true
         });
     },
 
-   'logoutHttp' : function(req, res) {
-      req.logout();
-      res.redirect('/');
-   },
+    'logoutHttp': function (req, res) {
+        req.logout();
+        res.redirect('/');
+    },
 
-    'local' : function(req, res) {
+    'localSocket': function (req, res) {
+        var username = req.body.displayName,
+            password = req.body.password,
+            isEmail;
+
+        if (!username || !password) {
+            return res.json({
+                error: true,
+                message: 'Incorrect display name or password'
+            });
+        }
+        isEmail = username.indexOf('@') !== -1;
+
+        User.findOne()
+            .where({
+                or: [
+                    {
+                        displayName: username,
+                        provider: 'local'
+                    },
+                    {
+                        email: username,
+                        provider: 'local'
+                    }
+                ]
+            })
+            .done(function (error, user) {
+                if (error) {
+                    return res.json({
+                        error: true,
+                        errorInstance: error
+                    });
+                } else if (!user) {
+                    return res.json({
+                        error: true,
+                        message: isEmail ? 'Incorrect email or password' : 'Incorrect display name or password'
+                    });
+                } else if (bcrypt.compareSync(password, user.encryptedPassword)) {
+                    req.logIn(user, function (err) {
+                        if (err) {
+                            console.log('login local err');
+                        }
+                        console.log('success login local web socket');
+                        return res.json({
+                            error: false,
+                            user: user
+                        });
+                    });
+
+                }
+            });
+    },
+
+    'local': function (req, res) {
         passport.authenticate('local', { failureRedirect: '/' },
             function (err, user) {
                 req.logIn(user, function (err) {
@@ -48,7 +102,7 @@ module.exports = {
 
     // http://developer.github.com/v3/
     // http://developer.github.com/v3/oauth/#scopes
-    'github' : function(req, res) {
+    'github': function (req, res) {
         passport.authenticate('github', { failureRedirect: '/' },
             function (err, user) {
                 req.logIn(user, function (err) {
@@ -64,11 +118,11 @@ module.exports = {
     },
 
 
-  /**
-   * Overrides for the settings in `config/controllers.js`
-   * (specific to AuthController)
-   */
-  _config: {}
+    /**
+     * Overrides for the settings in `config/controllers.js`
+     * (specific to AuthController)
+     */
+    _config: {}
 
-  
+
 };
--- testProject/clients/site/app/scripts/controllers/main.js
+++ testProject/clients/site/app/scripts/controllers/main.js
@@ -1,18 +1,44 @@
 'use strict';
 
 angular.module('siteApp')
-   .controller('MainCtrl', function ($scope, $state, user, csrf) {
+    .controller('MainCtrl', function ($scope, $state, user, csrf, $sails) {
 
-      user.subscribe($scope);
+        user.subscribe($scope);
 
-      $scope.csrf = csrf._csrf;
+        $scope.csrf = csrf._csrf;
 
-      $scope.isAuthenticated = function () {
-         return $scope.user ? true : false;
-      };
+        $scope.loginUser = {};
 
-      $scope.logout = function () {
-         return user.logout();
-      };
+        $scope.isAuthenticated = function () {
+            return $scope.user ? true : false;
+        };
 
-   });
+        $scope.logout = function () {
+            return user.logout();
+        };
+
+        $scope.loginErrorMsg = '';
+
+        $scope.hasLoginError = function() {
+            return $scope.loginErrorMsg.length > 0;
+        };
+
+        $scope.removeLoginErrorMsg = function() {
+            $scope.loginErrorMsg = '';
+        };
+
+        $scope.onLoginInputsChange = function() {
+            $scope.removeLoginErrorMsg();
+        };
+
+        $scope.signInLocal = function () {
+            $sails.get('/auth/localSocket', $scope.loginUser).success(function (data) {
+                if (!data.error) {
+                    user.login(data.user);
+                } else {
+                    $scope.loginErrorMsg = data.message;
+                }
+            });
+        };
+
+    });
